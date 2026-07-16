7e60c0f543f5feec27cd62d14999b94a95e0bc6d
bashvlas <bashvlas@gmail.com>
2026-07-16 17:24:45 +0200

    wip: react

--- packages/extension/src/entry/react/App.tsx
+++ packages/extension/src/entry/react/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { X, Menu, CircleFadingArrowUpIcon } from 'lucide-react';
+import { X, Menu, CircleFadingArrowUpIcon, Home, Settings } from 'lucide-react';
 import { Button } from '@src/components/ui/button';
+import logo_svg from '@shared/slots/logo-main.svg?raw';
 
 function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -19,6 +20,13 @@ const App = () => {
 
   function toggleMenu() {
     setMenuOpen(!menuOpen);
+  }
+
+  function handle_drawer_item_click(pageName: string) {
+    return () => {
+      setActivePageName(pageName);
+      setMenuOpen(false);
+    };
   }
   return (
     <div className='app'>
@@ -57,22 +65,25 @@ const App = () => {
       {/* Drawer */}
       <div className={cn('drawer', { 'drawer-open': menuOpen })} onClick={() => setMenuOpen(false)}>
         <div className='drawer-body'>
-          <div className='drawer-header'>drawer header</div>
-          <div className='drawer-content'>
-            <div>drawer content</div>
-            <div
-              onClick={() => {
-                setActivePageName('page1');
-              }}
-            >
-              page1
+          <div className='drawer-header'>
+            <div className='svg'>
+              <div dangerouslySetInnerHTML={{ __html: logo_svg }} />
             </div>
-            <div
-              onClick={() => {
-                setActivePageName('page2');
-              }}
-            >
-              page2
+          </div>
+          <div className='drawer-content'>
+            <div className='drawer-content-items'>
+              <div className={cn('drawer-content-item', { active: activePageName === 'page1' })} onClick={handle_drawer_item_click('page1')}>
+                <Home size={16} />
+                <span>Page 1</span>
+              </div>
+              <div className={cn('drawer-content-item', { active: activePageName === 'page2' })} onClick={handle_drawer_item_click('page2')}>
+                <Home size={16} />
+                <span>Page 2</span>
+              </div>
+              <div className={cn('drawer-content-item', { active: activePageName === 'settings' })} onClick={handle_drawer_item_click('settings')}>
+                <Settings size={16} />
+                <span>Settings</span>
+              </div>
             </div>
           </div>
         </div>
